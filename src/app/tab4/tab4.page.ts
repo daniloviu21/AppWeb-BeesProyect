0093fb45d9957d6d657b719ce96b07318a942d40
Tu Nombre <tuemail@example.com>
2025-02-17 19:19:01 -0600

    feat.logica taps4

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -8,7 +8,37 @@ import { NavController } from '@ionic/angular';
   standalone: false
 })
 export class Tab4Page {
+  darkMode = false;
 
+  
+
+  editarPerfil() {
+    console.log("Editar perfil");
+  }
+
+  cambiarContraseña() {
+    console.log("Cambiar contraseña");
+  }
+
+  metodosPago() {
+    console.log("Métodos de pago");
+  }
+
+  politicaPrivacidad() {
+    console.log("Política de privacidad");
+  }
+
+  terminosCondiciones() {
+    console.log("Términos y condiciones");
+  }
+
+  cerrarSesion() {
+    console.log("Cerrar sesión");
+  }
+
+  toggleDarkMode() {
+    document.body.classList.toggle('dark', this.darkMode);
+  }
   constructor( private navController: NavController ) { }
 
   navigateToLogin() {
