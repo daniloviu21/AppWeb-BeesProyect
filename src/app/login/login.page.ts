3456ffa72effb9c018e10e04517c2d4ac5bed267
Tu Nombre <tuemail@example.com>
2025-02-22 15:55:47 -0600

    fix(login): Navegacion

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ export class LoginPage {
   emailError: boolean = false;
   passwordError: boolean = false;
 
-  constructor(private toastController: ToastController) { }
+  constructor(private toastController: ToastController, private router: Router) { }
 
   validarEmail() {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -30,12 +31,12 @@ export class LoginPage {
       return;
     }
     if (this.emailError || this.passwordError) {
-      this.mostrarMensaje('Correo o Contraseña no valida ');
+      this.mostrarMensaje('Correo o Contraseña no válida');
       return;
     }
 
-    // Aquí iría la lógica de autenticación
     this.mostrarMensaje('Inicio de sesión exitoso');
+    this.router.navigate(['/tabs/tab1']);
   }
 
   async mostrarMensaje(mensaje: string) {
@@ -43,7 +44,7 @@ export class LoginPage {
       message: mensaje,
       duration: 2000,
       position: 'top',
-      color: 'danger',
+      color: 'success',
     });
     toast.present();
   }
