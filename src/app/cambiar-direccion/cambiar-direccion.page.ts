e636db01cfc913c8a1e176fd8ae5edab19e7b47e
Tu Nombre <tuemail@example.com>
2025-02-23 01:10:11 -0600

    feat(cambiar-direccion):contenido

--- src/app/cambiar-direccion/cambiar-direccion.page.ts
+++ src/app/cambiar-direccion/cambiar-direccion.page.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-cambiar-direccion',
   templateUrl: './cambiar-direccion.page.html',
-  styleUrls: ['./cambiar-direccion.page.scss'],
-  standalone: false
+  styleUrls: ['./cambiar-direccion.page.scss']
 })
 export class CambiarDireccionPage implements OnInit {
 
-  constructor() { }
+  direccion = {
+    direccion: '',
+    numInterior: '',
+    numExterior: '',
+    codigoPostal: '',
+    localidad: '',
+    telefono: '',
+    ordenesEntrega: ''
+  };
+
+  constructor(private router: Router, private usuariosService: UsuariosService) { }
 
   ngOnInit() {
+    this.cargarDatosGuardados();
   }
 
+  volverAInicio() {
+    this.router.navigate(['/tabs/tab4']);
+  }
+
+  async agregarDireccion() {
+    let usuario = this.usuariosService.getUsuario();
+    if (usuario) {
+      usuario.direccion = this.direccion.direccion;
+      usuario.telefono = this.direccion.telefono;
+      await this.usuariosService.saveCurrentUser();
+      alert('Dirección guardada exitosamente');
+    } else {
+      alert('No hay usuario autenticado');
+    }
+  }
+
+  async cargarDatosGuardados() {
+    let usuario = this.usuariosService.getUsuario();
+    if (usuario && usuario.direccion) {
+      this.direccion.direccion = usuario.direccion;
+      this.direccion.telefono = usuario.telefono || '';
+    }
+  }
 }
