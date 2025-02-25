7141a592c34de19f4f163ad2b7a18dbe2fbed6fb
jrvrr <moralesriveraj10@gmail>
2025-02-25 00:25:50 -0600

    feat: funcion de servicios

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,6 +10,22 @@ import { Categoria } from '../services/productos.service';
   standalone: false,
 })
 export class Tab1Page implements OnInit {
+  
+  currentIndexServ: number = 0;
+  servicios = [
+    { image: 'assets/icon/carrusel/servicio1.jpg', title: 'Corte y Grabado Láser', description: 'Precisión y detalle en cada corte y grabado para personalizar tus anuncios.' },
+    { image: 'assets/icon/carrusel/servicio2.jpg', title:'Impresión', description:'Impresión de alta calidad para todos tus materiales publicitarios.' },
+    { image: 'assets/icon/carrusel/servicio3.jpg', title:'Rotulación', description:'Rotulación profesional para vehículos, vitrinas y más.' },
+    { image: 'assets/icon/carrusel/servicio4.jpg', title:'Mantenimiento', description:'Servicios de mantenimiento para asegurar que tus anuncios siempre luzcan impecables.' },
+  ];
+
+  prevSlideServ() {
+    this.currentIndexServ = (this.currentIndexServ - 1 + this.servicios.length) % this.servicios.length;
+  }
+
+  nextSlideServ() {
+    this.currentIndexServ = (this.currentIndexServ + 1) % this.servicios.length;
+  }
 
   @Input() address: string = 'Cordoba Veracruz';
   @Input() cartCount: number = 0;
