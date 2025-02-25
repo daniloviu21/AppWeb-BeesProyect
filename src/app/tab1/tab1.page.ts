acc5a84253d9356e9fe0aa91ff56c5d83392ca7f
jrvrr <moralesriveraj10@gmail>
2025-02-24 21:54:30 -0600

    Feat: Tab1: Direccion de la empresa y carrito

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Usuario } from '../services/usuarios.service';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,10 +10,16 @@ import { Usuario } from '../services/usuarios.service';
 })
 export class Tab1Page implements OnInit{
 
-  @Input() usuario!: Usuario;
+  @Input() address: string = 'Cordoba México';
+  @Input() cartCount: number = 0;
 
-  constructor() { }
+  constructor(private platform: Platform) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
-  ngOnInit() {}
-
+  openMaps() {
+    const mapsUrl = `https://www.google.com/maps/dir/18.8142666,-96.7253164/18.92019,-96.96397/@18.8674233,-97.0139553,11z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D`;
+    window.open(mapsUrl, '_blank');
+  }
 }
