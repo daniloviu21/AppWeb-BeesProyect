280753b856eab04f90ef79fe1225bb6bfd90405f
Tu Nombre <tuemail@example.com>
2025-02-28 21:08:40 -0600

    fix(app-routing): Boton direcciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,27 @@ const routes: Routes = [
     path: 'productos/:categoria',
     loadChildren: () => import('./pages/productos/productos.module').then( m => m.ProductosPageModule)
   },
+  {
+    path: 'editar-perfil',
+    loadChildren: () => import('./editar-perfil/editar-perfil.module').then( m => m.EditarPerfilPageModule)
+  },
+  
+  {
+    path: 'metodos-pago',
+    loadChildren: () => import('./metodos-pago/metodos-pago.module').then( m => m.MetodosPagoPageModule)
+  },
+  {
+    path: 'terminos-condiciones',
+    loadChildren: () => import('./terminos-condiciones/terminos-condiciones.module').then( m => m.TerminosCondicionesPageModule)
+  },
+  { 
+    path: 'cambiar-direccion',
+    loadChildren: () => import('./cambiar-direccion/cambiar-direccion.module').then( m => m.CambiarDireccionPageModule)
+  },
+  {
+    path: 'terminos-condiciones',
+    loadChildren: () => import('./terminos-condiciones/terminos-condiciones.module').then( m => m.TerminosCondicionesPageModule)
+  }
   
 ];
 @NgModule({
