41ee6d97c4023866e0456b37712001caacd33ae9
jrvrr <moralesriveraj10@gmail>
2025-03-06 23:19:37 -0600

    fix: errores en funciones y tamaños

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.configureStatusBar();
+  }
+
+  async configureStatusBar() {
+    await StatusBar.setOverlaysWebView({ overlay: false });
+    await StatusBar.setBackgroundColor({ color: '#000000' });
+  }
 }
